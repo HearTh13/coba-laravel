e8772e2c65d179ea368bd4c060f6abd4cf5453ec
HearTh13 <gun123sokero@gmail.com>
2024-06-06 08:11:58 +0700

    Changes to be committed: modified:   app/Models/Post.php modified:   resources/views/post.blade.php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <article class="mb-5 mt-5 text-dark">
         <h1 class="text-dark"> {{$post->title}} </h1>
+        <p>By. Achmad Herlambang</p>
         {!! $post->body !!}
     </article>
     <a href="/posts">Back</a>
